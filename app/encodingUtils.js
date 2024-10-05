b1d1a0ddbba892a343e3f7e3acca889453d2b4fc
Bhushan C. Mhatre <44663470+thebhushanmhatre@users.noreply.github.com>
2024-10-05 23:38:43 +0530

    encoding the info dict and printing its hash value

---
+++ app/encodingUtils.js
@@ -0,0 +1,32 @@
+// Strings are encoded as <length>:<contents>.
+// Integers are encoded as i<number>e.
+// Lists are encoded as l<bencoded_elements>e.
+// A dictionary is encoded as d<key1><value1>...<keyN><valueN>e.
+
+// info dictionary format:
+//
+// {
+//   length: 92063,
+//   name: 'sample.txt',
+//   'piece length': 32768,
+//   pieces: `xyz`
+// }
+
+function encodeInBencode(data) {
+  let result = '';
+  Object.entries(data).map(([key, value]) => {
+    let encodedKey = `${key.length}:${key}`;
+    let encodedValue;
+    // data can be integer
+    if (!isNaN(value)) {
+      encodedValue = `i${value}e`;
+    } else {
+      encodedValue = `${value.length}:${value}`;
+    }
+    result += encodedKey + encodedValue;
+  });
+
+  return result;
+}
+
+module.exports = { encodeInBencode };
--- app/main.js
+++ app/main.js
@@ -1,12 +1,14 @@
 const process = require('process');
 const util = require('util');
 const fs = require('fs');
+const crypto = require('crypto');
 const {
   decodeStr,
   decodeInteger,
   decodeList,
   decodeDictionary,
 } = require('./decodingUtils');
+const { encodeInBencode } = require('./encodingUtils');
 
 function decodeBencode(bencodedValue) {
   const lastChar = bencodedValue[bencodedValue.length - 1];
@@ -34,6 +36,15 @@ function main() {
     // In JavaScript, there's no need to manually convert bytes to string for printing
     // because JS doesn't distinguish between bytes and strings in the same way Python does.
     console.log(JSON.stringify(decodeBencode(bencodedValue)));
+  } else if (command === 'encode') {
+    const dataObjectToEncode = {
+      url: 'http://bittorrent-test-tracker.codecrafters.io/announce',
+      length: '92063',
+      name: 'sample.txt',
+      'piece length': 32768,
+    };
+    const encodedData = encodeInBencode(dataObjectToEncode);
+    console.log(encodedData);
   } else if (command === 'info') {
     const filePath = process.argv[3];
     fs.readFile(filePath, 'utf8', (err, data) => {
@@ -45,6 +56,14 @@ function main() {
       const decodedData = decodeBencode(data);
       console.log('Tracker URL:', decodedData.announce);
       console.log('Length:', decodedData.info.length);
+
+      const bencodedInfoDict = encodeInBencode(decodedData.info);
+
+      const shasum = crypto.createHash('sha1');
+      shasum.update(bencodedInfoDict);
+      const digestResult = shasum.digest('hex');
+
+      console.log('Info Hash:', digestResult);
     });
   } else {
     throw new Error(`Unknown command ${command}`);
