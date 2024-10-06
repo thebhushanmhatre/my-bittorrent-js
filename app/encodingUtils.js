89baa179ebc7a5c8e1d4280f20c65c43d69cbf5c
Bhushan C. Mhatre <44663470+thebhushanmhatre@users.noreply.github.com>
2024-10-06 13:51:48 +0530

    Converting encode into a generic function

--- app/encodingUtils.js
+++ app/encodingUtils.js
@@ -12,16 +12,51 @@
 //   pieces: `xyz`
 // }
 
+function encodeStr(input) {
+  return `${input.length}:${input}`;
+}
+
+function encodeInteger(input) {
+  return `i${input}e`;
+}
+
+function encodeList(input) {
+  'l' +
+    value
+      .map((item) => {
+        if (!isNaN(item)) {
+          return encodeInteger(item);
+        } else if (typeof item == 'string') {
+          return encodeStr(item);
+        } else if (Array.isArray(item)) {
+          return encodeList(item);
+        } else {
+          return encodeInBencode(item);
+        }
+      })
+      .join('') +
+    'e';
+}
+
 function encodeInBencode(data) {
   let result = '';
-  Object.entries(data).map(([key, value]) => {
-    let encodedKey = `${key.length}:${key}`;
+  const keys = Object.keys(data).sort((a, b) => a.localeCompare(b));
+
+  keys.map((key) => {
+    const value = data[key];
+    let encodedKey = encodeStr(key);
     let encodedValue;
     // data can be integer
     if (!isNaN(value)) {
-      encodedValue = `i${value}e`;
+      encodedValue = encodeInteger(value);
+    } else if (typeof value == 'string') {
+      encodedValue = encodeStr(value);
+    } else if (Array.isArray(value)) {
+      // array - l<bencoded_elements>e
+      encodedValue = encodeList(value);
     } else {
-      encodedValue = `${value.length}:${value}`;
+      // nested dict - d<key1><value1>...<keyN><valueN>e
+      encodedValue = encodeInBencode(value);
     }
     result += encodedKey + encodedValue;
   });
